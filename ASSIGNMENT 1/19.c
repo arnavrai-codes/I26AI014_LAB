17455f6a0be7b66083381a30fb30df358dafcacd
arnavrai-codes <raiarnav751@gmail.com>
2026-09-24 21:05:32 +0530

    Delete ASSIGNMENT 1 directory

--- ASSIGNMENT 1/19.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-char c;
-printf("enter character:");
-scanf("%c", &c);
-(c>=97 && c<=122)? printf("small case character"):printf("not small case character");
-}
--- ASSIGNMENT 1/evaluation 1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int p,r,t;
-    printf("enter p,r,t:");
-    scanf("%d%d%d",&p,&r,&t);
-    printf("simple intrest is:%d",(p*r*t)/100);
-    return 0;
-}
